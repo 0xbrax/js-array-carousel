2452f063224c6e03433597263e08581cbbda140d
Marco Braccini <marco.braccini@live.it>
2022-07-08 19:33:41 +0200

    @END Project - Add js program To do: bonus click & bonus keyboard input

--- js/main.js
+++ js/main.js
@@ -14,7 +14,8 @@ for (let i = 1; i < img.length; i++) {
     items.innerHTML += `<div class="item-img"><img class="main-img" src="${img[i]}"></div>`;
 }
 
-for (let i = 0; i < img.length; i++) {
+thumb.innerHTML += `<div class="thumb-img"><img class="sec-img active" src="${img[0]}"></div>`;
+for (let i = 1; i < img.length; i++) {
     thumb.innerHTML += `<div class="thumb-img"><img class="sec-img" src="${img[i]}"></div>`;
 }
 
@@ -23,3 +24,39 @@ thumb.innerHTML += `<div id="arrow-down"><i class="fa-solid fa-circle-chevron-do
 
 
 
+const arrowUp = document.getElementById('arrow-up');
+const arrowDown = document.getElementById('arrow-down');
+let imgMain = document.getElementsByClassName('item-img');
+let imgSec = document.querySelectorAll('.sec-img');
+
+let imgSelected = 0;
+
+arrowDown.addEventListener('click', function() {
+    if (imgSelected == img.length - 1) {
+        imgMain[imgSelected].classList.remove('show');
+        imgSec[imgSelected].classList.remove('active');
+        imgSelected = 0;
+    } else if (imgSelected >= 0) {
+        imgMain[imgSelected].classList.remove('show');
+        imgSec[imgSelected].classList.remove('active');
+        imgSelected++;
+    }
+
+    imgMain[imgSelected].classList.add('show');
+    imgSec[imgSelected].classList.add('active');
+});
+
+arrowUp.addEventListener('click', function() {
+    if (imgSelected == 0) {
+        imgMain[imgSelected].classList.remove('show');
+        imgSec[imgSelected].classList.remove('active');
+        imgSelected = img.length - 1;
+    } else if (imgSelected > 0) {
+        imgMain[imgSelected].classList.remove('show');
+        imgSec[imgSelected].classList.remove('active');
+        imgSelected--;
+    }
+
+    imgMain[imgSelected].classList.add('show');
+    imgSec[imgSelected].classList.add('active');
+});
